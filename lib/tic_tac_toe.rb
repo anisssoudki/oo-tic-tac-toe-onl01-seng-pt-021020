be2b11fdaae885a7278da0cfdce465a39f9108be
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-03-13 18:07:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -258,7 +258,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
     else
       nil
     end
-  end
+  
   
   def play
     self.turn while !self.over?
@@ -271,7 +271,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
    turn until draw? || over? || won?
    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
  end
-end
+
 
 
 end
